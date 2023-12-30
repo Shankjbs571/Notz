0585563f0fe554b86d2324f7f3f62c189bc074dc
Shashank Kumar <shankjbs571@gmail.com>
2023-12-31 02:08:55 +0530

    new

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, file_names
 
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
 
 import 'ToDoTile.dart';
@@ -12,21 +14,37 @@ class Homepage extends StatefulWidget {
 }
 
 class _Homepagestate extends State<Homepage> {
+  List notzlist = [
+    ["task1", true],
+    ["task2", false]
+  ];
+
+  void checkboxchanged(bool? value, int index) {
+    setState(() {
+      notzlist[index][1] = !notzlist[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 255, 240, 107),
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 80, 77, 51),
-          title: Text('T O  D O'),
-        ),
-        body: Padding(
+      backgroundColor: Color.fromARGB(255, 208, 208, 208),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        title: Text('Notz'),
+      ),
+      body: Padding(
           padding: const EdgeInsets.all(25),
-          child: ListView(
-            children: [
-              ToDoTile(),
-            ],
-          ),
-        ));
+          child: ListView.builder(
+            itemCount: notzlist.length,
+            itemBuilder: (context, index) {
+              return ToDoTile(
+                taskName: notzlist[index][0],
+                taskCompleted: notzlist[index][1],
+                onChanged: (value) => checkboxchanged(value, index),
+              );
+            },
+          )),
+    );
   }
 }
--- lib/ToDoTile.dart
+++ lib/ToDoTile.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  ToDoTile(
+      {super.key,
+      required this.taskName,
+      required this.taskCompleted,
+      required this.onChanged});
+
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Task1"),
+    return Padding(
+      padding: const EdgeInsets.all(25),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        height: 70,
+        child: Row(
+          children: [
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+            Text(taskName),
+          ],
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 3,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
